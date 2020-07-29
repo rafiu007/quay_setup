1582dee4f103dda2e9a3be86f0cc950f8c13583d
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-07-29 13:18:40 +0530

    feat: Setup script for quay, clair, cso

---
+++ quay_setup.sh
@@ -0,0 +1,18 @@
+# oc create -f https://raw.githubusercontent.com/minio/minio-operator/master/minio-operator.yaml
+# install quay operator
+oc apply -f quay-op.yaml
+# install cso operator
+oc apply -f cso.yaml
+# add pull secrete for quay images
+oc create secret generic redhat-pull-secret --from-file=".dockerconfigjson=docker_quay.json" --type='kubernetes.io/dockerconfigjson'
+# create quay instance
+oc apply -f quayecosystem.yaml
+# enable clair v4 scanning for clairv4 org
+# this has to be done after creation of quay-enterprise-config-secret
+oc patch secret quay-enterprise-config-secret -p "$(oc get secrets quay-enterprise-config-secret -o json | jq -r '.data["config.yaml"] | @base64d | . + "SECURITY_SCANNER_V4_ENDPOINT: http://clairv4\nSECURITY_SCANNER_V4_NAMESPACE_WHITELIST:\n- clairv4\n\n"| @base64 | {"data":{"config.yaml": .}}')"
+# add clair v4 config
+oc create secret generic con-clair --from-file=./config.yaml
+# clair v4 supporting services
+oc apply -f jager.yaml
+oc apply -f clairv4_db.yaml
+oc apply -f clairv4.yaml
