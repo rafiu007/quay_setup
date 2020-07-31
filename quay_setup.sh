db9eedaf4e3a65eb0864ae32fb906a98a2c5129a
Rafiu Mollah <rafiu@localhost.localdomain>
2020-07-31 18:37:43 +0530

    (use "git push" to publish your local commits)

--- quay_setup.sh
+++ quay_setup.sh
@@ -8,9 +8,20 @@ oc apply -f cso.yaml
 # add pull secrete for quay images
 oc create secret generic redhat-pull-secret --from-file=".dockerconfigjson=docker_quay.json" --type='kubernetes.io/dockerconfigjson'
 #CSO takes a lot of time to come up so need to wait before bringing up quayecosystem
-sleep 2m
+#sleep 5m
+#code to wait for CSO to come up
+oc get QuayEcosystem
+while true; do
+  oc get QuayEcosystem
+  if [ "$?" == "0" ]; then
+    oc apply -f quayecosystem.yaml
+    break
+  fi
+  sleep 1
+done
 # create quay instance
-oc apply -f quayecosystem.yaml
+#oc apply -f quayecosystem.yaml
+
 # enable clair v4 scanning for clairv4 org
 # this has to be done after creation of quay-enterprise-config-secret
 oc patch secret quay-enterprise-config-secret -p "$(oc get secrets quay-enterprise-config-secret -o json | jq -r '.data["config.yaml"] | @base64d | . + "SECURITY_SCANNER_V4_ENDPOINT: http://clairv4\nSECURITY_SCANNER_V4_NAMESPACE_WHITELIST:\n- clairv4\n\n"| @base64 | {"data":{"config.yaml": .}}')"
