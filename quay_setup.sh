7bff257367245cb2ef918368e166c39bb97df2f0
Rafiu Mollah <rafiu@localhost.localdomain>
2020-08-11 19:04:44 +0530

    Added a function to deal with both corner cases

--- quay_setup.sh
+++ quay_setup.sh
@@ -1,4 +1,31 @@
 # oc create -f https://raw.githubusercontent.com/minio/minio-operator/master/minio-operator.yaml
+
+check(){
+  eval VAR1="$1"
+  eval VAR2="$2"
+  while true; do
+    if [[ $3 == "1" ]]
+    then
+      VAR3=$(eval $VAR1 2>&1)
+      echo "Waiting for  secret......................"
+      echo $VAR3
+      if [[ $VAR3 != $VAR2 ]]
+      then
+        echo "Secret found..........................."
+        break
+      fi
+    else
+      VAR3=$($VAR1 2>&1)
+      echo "Checking for QuayEcosystem................"
+      if [[ $VAR3 == $VAR2 ]]
+      then
+        break
+      fi
+    fi
+    sleep 1
+  done
+}
+
 #Create a Project
 oc new-project quay-enterprise
 # install quay operator
@@ -12,30 +39,15 @@ oc create secret generic redhat-pull-secret --from-file=".dockerconfigjson=docke
 
 #code to wait for quay-operator to come up
 req="No resources found in quay-enterprise namespace."
-while true; do
-  echo "Waiting for QuayEcosystem...."
-  VAR1=$(oc get QuayEcosystem 2>&1)
-  if [[ $VAR1 == $req ]]
-  then
-    echo "QuayEcosystem found"
-    oc apply -f quayecosystem.yaml
-    break
-  fi
-  sleep 1
-done
+com="oc get QuayEcosystem"
+check "\${com}" "\${req}"
+oc apply -f quayecosystem.yaml
 # create quay instance
 #oc apply -f quayecosystem.yaml
 #Wait for secret to get populated
-while true; do
-  echo "Looking for quay-enterprise-config-secret...."
-  VAL2=$(oc get secrets quay-enterprise-config-secret -o json | jq -r '.data["config.yaml"]' 2>&1)
-  if [[ $VAL2 != "null" ]]
-  then
-    echo "Secret found."
-    break
-  fi
-  sleep 1
-done
+req="null"
+com="oc get secrets quay-enterprise-config-secret -o json | jq -r '.data[\"config.yaml\"]'"
+check "\${com}" "\${req}" "1"
 # enable clair v4 scanning for clairv4 org
 # this has to be done after creation of quay-enterprise-config-secret
 oc patch secret quay-enterprise-config-secret -p "$(oc get secrets quay-enterprise-config-secret -o json | jq -r '.data["config.yaml"] | @base64d | . + "SECURITY_SCANNER_V4_ENDPOINT: http://clairv4\nSECURITY_SCANNER_V4_NAMESPACE_WHITELIST:\n- clairv4\n\n"| @base64 | {"data":{"config.yaml": .}}')"
