f1f2a5138ec5c1b8cefcb27768c183e80db6d20d
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-10-20 12:24:32 +0530

    Fix jq error when running pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -4,7 +4,7 @@ HOSTED_ZONE_ID=$1
 NAME=$2
 JSON_FILE="del-records.json"
 echo "Deleting DNS Record set"
-aws route53 list-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --output json | jq '[.ResourceRecordSets[] | select(.Name | test("${Name}")) | {ResourceRecordSet: ., Action: "DELETE"}] | {Changes: ., Comment: "Delete record sets for ${NAME}"}' > $JSON_FILE
+aws route53 list-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --output json | jq --arg Name $NAME '[.ResourceRecordSets[] | select(.Name | test($Name)) | {ResourceRecordSet: ., Action: "DELETE"}] | {Changes: ., Comment: "Delete record sets for $Name"}' > $JSON_FILE
 aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file://$JSON_FILE >/dev/null
 echo "Deleting record set ..."
 rm -fr $JSON_FILE
